f843a325006e20b330448af609f9c97b17748b36
Rinat <rinat1kam@mail.ru>
2020-12-12 03:30:45 +0500

    slider

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,14 @@
-"use strict";
+const slider = tns({
+    container: '.slider__inner',
+    items: 1,
+    slideBy: 'page',
+    controls: false
+});
+
+document.querySelector('.prev').addEventListener('click', function () {
+    slider.goTo('prev');
+});
+
+document.querySelector('.next').addEventListener('click', function () {
+    slider.goTo('next');
+});
